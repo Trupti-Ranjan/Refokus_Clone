f2e13e19ac8775e70afcc2653b429396a9b42675
Trupti Ranjan Barik <adarshbarik563@gmail.com>
2025-05-02 11:43:34 +0530

    finally fix the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='max-w-[1130px] py-6 mx-auto flex flex-col md:flex-row gap-4 md:gap-20 items-center border-b-[.5px] border-zinc-700 overflow-visible px-4 md:px-0'>
-      <div className='w-full md:w-auto flex items-center justify-between'>
+    <div className='max-w-[1130px] py-6 mx-auto flex flex-col md:flex-row gap-4 md:gap-20 items-center justify-between border-b-[.5px] border-zinc-700 overflow-visible px-4 md:px-0'>
+      <div className='w-full md:w-auto flex items-center justify-between gap-16'>
         <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" className='w-32 md:w-auto' />
         <button
           className='md:hidden text-white'
@@ -21,11 +21,8 @@ const Navbar = () => {
             )}
           </svg>
         </button>
-      </div>
 
-      <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row gap-4 md:gap-14 items-center w-full md:w-auto`}>
-        <div className='flex justify-between gap-7 w-[97`0px]'>
-
+        <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row gap-4 md:gap-14 items-center w-full md:w-auto`}>
           <div className='flex flex-col md:flex-row gap-4 md:gap-14 items-center justify-between'>
             {["Home", "Work", "Career", "", "News"].map((ele, index) => ele.length === 0 ?
               <span key={index} className='hidden md:block w-[.5px] h-5 bg-zinc-600'></span> :
@@ -35,10 +32,12 @@ const Navbar = () => {
               </Link>
             )}
           </div>
-          <div className='mt-4 md:mt-0'>
-            <Button value={"Start a Project"} />
-          </div>
         </div>
+      </div>
+
+      
+      <div className='mt-4 md:mt-0'>
+        <Button value={"Start a Project"} />
       </div>
     </div>
   )
